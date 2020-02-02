853a5f5ec77be0f690b910fd0fb0c31164c00953
arkeane <ludovicopestarino00@gmail.com>
2020-02-02 23:20:45 +0100

    Polish code

--- polybar_autohide.cpp
+++ polybar_autohide.cpp
@@ -5,44 +5,49 @@
 #include<sstream>
 #include<string>
 #include<fstream>
-#include <X11/Xlib.h>
-#include <X11/Xutil.h>
-using namespace std;
+#include<X11/Xlib.h>
+#include<X11/Xutil.h>
 
-//shell scripts
+// Shell scripts
 #define windowlist "xdotool search --all --onlyvisible --desktop $(xprop -notype -root _NET_CURRENT_DESKTOP | cut -c 24-) \"\" 2>/dev/null | tr -d 'Defaulting to search window name, class, and classname' > .windowlist"
 #define windowcount "wc -l .windowlist | tr -d ' .windowlist'"
 #define show "xdo show -N Polybar"
 #define hide "xdo hide -N Polybar"
 
-//variables
+// Variables
 #define polybar_height 30
 
 void togglefile(){
-	//inizialize toggle file
-	fstream fout;
-	fout.open(".togglefile", ios::out);
+	// Inizialize toggle file
+	std::fstream fout;
+	fout.open(".togglefile", std::ios::out);
 	fout << 0;
 	fout.close();
 }
 
-//read shell command and put in a string string
-string GetStdoutFromCommand(string cmd) {
-    string data;
+void keybinding(int& k){
+	// Read toggle file
+	std::fstream fin;
+	fin.open(".togglefile", std::ios::in);
+	fin >> k;
+	fin.close();
+}
+
+void GetStdoutFromCommand(std::string cmd, std::string &data) {
+	// Read shell command and put in a string
     FILE * stream;
     const int max_buffer = 256;
     char buffer[max_buffer];
     cmd.append(" 2>&1");
     stream = popen(cmd.c_str(), "r");
-    if (stream) {
-    while (!feof(stream))
-    if (fgets(buffer, max_buffer, stream) != NULL) data.append(buffer);
-    pclose(stream);
+    if(stream){
+    	while(!feof(stream))
+    	if(fgets(buffer, max_buffer, stream) != NULL) data.append(buffer);
+    	pclose(stream);
     }
-return data;
 }
 
-void getpointerY(int &y){
+void getpointer(int &y){
 	// Setup display and such
     char *display_name = getenv("DISPLAY");
     Display *display = XOpenDisplay(display_name);
@@ -52,60 +57,58 @@ void getpointerY(int &y){
     int win_x, root_x, root_y = 0;
     unsigned int mask = 0;
     Window child_win, root_win;
-    XQueryPointer(display, XRootWindow(display, screen),
-        &child_win, &root_win,
-        &root_x, &root_y, &win_x, &y, &mask);
+    XQueryPointer(display, XRootWindow(display, screen), &child_win, &root_win, &root_x, &root_y, &win_x, &y, &mask);
 }
 
-void windowcounter(unsigned int& w){
+void windowcounter(int &w){
+	// Get active windows number as string
 	system(windowlist);
-	string result;
-	result = GetStdoutFromCommand(windowcount);
-	int num;
+	std::string result;
+	GetStdoutFromCommand(windowcount, result);
+
+	// Convert string to int
 	std::istringstream iss (result);
-	iss >> num;
-	w = num;
+	iss >> w;
 }
 
-void keybinding(int& k){
-	fstream fin;
-	fin.open(".togglefile", ios::in);
-	fin >> k;
-	fin.close();
-}
-
-int main(){
-	//inizialization
+int main(int argc, char* argv[]){
+	// Inizialization
 	togglefile();
-	int y, k = 0;
-	unsigned int w = 0;
-	//Infinite loop
+	int k, w, y = 0;
+
+	// Infinite Loop
 	while(true){
-		// detect keybinding
+		// Detect toggle
 		keybinding(k);
-		// get y pointer
-		getpointerY(y);
-		// detect window presence
+		// Get y pointer
+		getpointer(y);
+		// Get windows number
 		windowcounter(w);
+
+		// Activation hierarchy
+		// Toggle active = 1
 		if(k == 1){
 			system(show);
 		}
+		// Toggle not active = 0
 		else{
+			// Mouse pointer near the bar
 			if(y <= polybar_height){
 				system(show);
 			}
+			// Mouse pointer not near the bar
 			else{
-				//check if windows are displayed
+				// No active windows
 				if(w == 0){
-					//cout << w << endl;
 					system(show);
 				}
+				// 1 or more active windows
 				else{
-					//cout << w << endl;
 					system(hide);
 				}
 			}
 		}
+		// Delay
 		sleep(1);
 	}
 return 0;
