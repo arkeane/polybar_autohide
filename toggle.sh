6db749ded294b81e8e01f6edafae1aadf9c46e05
Ludovico Pestarino <47981941+arkeane@users.noreply.github.com>
2021-02-21 10:11:12 +0100

    Delete toggle.sh

--- toggle.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# DISABLED
-TOGGLEFILE="$HOME/.cache/polybar-togglefile"
-line=$(head -n 1 "$TOGGLEFILE")
-if [ $line = 0 ]; then
-    echo 1 > "$TOGGLEFILE"
-else
-    echo 0 > "$TOGGLEFILE"
-fi
